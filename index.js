46d4658d06f77b208f81279b73d606a05b704e3e
Wizardbacon13 <longphan0619@gmail.com>
2024-10-20 01:11:42 -0500

    fix: added tutorial message for semantic release

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
 // In case if you forget how to use semantic release, use this url:
 // https://www.youtube.com/watch?v=mah8PV6ugNY
+// In summary: 
+// 1. do the command "npm install semantic-release @semantic-release/git @semantic-release/github -D"
+// 2. Optionally create a release.config.js file for configuration options, but must create a github workflow (.github/workflows/release.yml)
+// 3. Enable read and write permissions, either through the GitHub website or through code in the release.yml file (see youtube video)
+// 4. Done!
+
 const thisIsAFunction = "this is a test var!";
 
 console.log(thisIsAFunction);
