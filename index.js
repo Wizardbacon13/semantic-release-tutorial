53bc4e2fe23a3a685bf7862c075596b56b3f86c3
Wizardbacon13 <longphan0619@gmail.com>
2024-10-20 01:00:11 -0500

    feat: Added release.config.js

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+// In case if you forget how to use semantic release, use this url:
+// https://www.youtube.com/watch?v=mah8PV6ugNY
 const thisIsAFunction = "this is a test var!";
 
 console.log(thisIsAFunction);
